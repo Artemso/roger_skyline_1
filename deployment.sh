3f7dc3fa0e5191b1ce70b0ae86bb716b48236126
Artemso <solopovtema@gmail.com>
2020-03-06 12:24:35 +0200

    updates

--- deployment.sh
+++ deployment.sh
@@ -6,7 +6,7 @@
 #    By: asolopov <asolopov@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/06 12:09:02 by asolopov          #+#    #+#              #
-#    Updated: 2020/03/06 12:17:59 by asolopov         ###   ########.fr        #
+#    Updated: 2020/03/06 12:24:31 by asolopov         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,4 +22,19 @@ error_exit() {
 
 echo -e "${GREEN}-----Updating system-----${RES}"
 bash srcs/update_packages.sh || error_exit
+echo -e "${GREEN}-----Done-----${RES}"
+
+declare -a packages=(
+	"vim"
+	"ufw"
+	"fail2ban"
+	"postfix"
+	"mailutils"
+	"apache2"
+)
+
+for p in ${packages[@]}; do
+	echo -e "${GREEN}-----Installing packages-----${RES}"
+	apt-get install -y $p
+done
 echo -e "${GREEN}-----Done-----${RES}"
