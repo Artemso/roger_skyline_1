afb5d4a5003052f40e20e5feacd636c58a2b5572
Artemso <solopovtema@gmail.com>
2020-03-07 16:52:27 +0200

    push

--- srcs/update_html.sh
+++ srcs/update_html.sh
@@ -6,11 +6,11 @@
 #    By: asolopov <asolopov@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/07 15:48:46 by asolopov          #+#    #+#              #
-#    Updated: 2020/03/07 16:49:22 by asolopov         ###   ########.fr        #
+#    Updated: 2020/03/07 16:52:11 by asolopov         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-cd /home/dev && git pull
+cd /home/deploy && git pull
 
 sumnew=($(md5sum "/home/index.html"))
 sumold=($(cat "/home/html_md5" | awk '{ print $1 }'))
