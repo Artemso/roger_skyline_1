dee6ecd630ece48b1a1c2f05da03526dd61da68b
Artemso <solopovtema@gmail.com>
2020-03-07 16:00:03 +0200

    upload

--- srcs/update_html.sh
+++ srcs/update_html.sh
@@ -6,7 +6,7 @@
 #    By: asolopov <asolopov@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/07 15:48:46 by asolopov          #+#    #+#              #
-#    Updated: 2020/03/07 15:55:54 by asolopov         ###   ########.fr        #
+#    Updated: 2020/03/07 15:59:59 by asolopov         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,8 @@ git pull
 
 sumnew=($(md5sum "srcs/index.html"))
 sumold=($(cat "/home/html_md5" | awk '{ print $1 }'))
-
+echo "$sumnew"
+echo "$sumold"
 if [ "$sumnew" != "$sumorg" ]
 then
 	echo "Different"
